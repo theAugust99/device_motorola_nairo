f3d8743330113d6d0b48f3a3bd877fc7190b13c5
theAugust99 <Gutobarroszk@gmail.com>
2022-07-13 23:21:11 +0000

    Bringup to dot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nairo.mk
+    $(LOCAL_DIR)/dot_nairo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nairo-eng \
-    lineage_nairo-user \
-    lineage_nairo-userdebug
+    dot_nairo-eng \
+    dot_nairo-user \
+    dot_nairo-userdebug
--- dot_nairo.mk
+++ dot_nairo.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/nairo/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_nairo
+PRODUCT_NAME := dot_nairo
 PRODUCT_DEVICE := nairo
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto G 5G Plus
